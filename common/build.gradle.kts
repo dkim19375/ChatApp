e36ee21c24b95d4cc73f82f68e86afe849e86e4a
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2023-08-01 17:19:50 +0900

    add guava and commons-io

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies{
+    api("commons-io:commons-io:2.13.0")
+    api("com.google.guava:guava:32.1.1-jre")
     api("com.google.code.gson:gson:2.10.1")
     api("io.github.dkim19375:dkimcore:1.4.2")
     api("ch.qos.logback:logback-classic:1.4.8")
