f5cd69e20943778a4e8b22b50007a8d76d8664f8
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2023-08-01 17:17:12 +0900

    Remove old dependency

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,7 +2,6 @@ dependencies{
     api("com.google.code.gson:gson:2.10.1")
     api("io.github.dkim19375:dkimcore:1.4.2")
     api("ch.qos.logback:logback-classic:1.4.8")
-    api("uk.org.lidalia:sysout-over-slf4j:1.0.2")
     api("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 }
